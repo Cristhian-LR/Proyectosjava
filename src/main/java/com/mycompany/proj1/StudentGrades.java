3fd85b006527c8be35eee104faa03c3ebdab9504
cristhian <15186@ulsachihuahua.edu.mx>
2025-02-11 10:36:01 -0600

    agregacion de comentarios

--- src/main/java/com/mycompany/proj1/StudentGrades.java
+++ src/main/java/com/mycompany/proj1/StudentGrades.java
@@ -11,20 +11,20 @@ package com.mycompany.proj1;
 public class StudentGrades {
 
     public int calculatePartialGrade(int examScore, int homeworkScore) {
-        double firstPartialGrade = (examScore * 0.7) + (homeworkScore * 0.3);//calcula la calificacion parcial
+        double firstPartialGrade = (examScore * .7) + (homeworkScore * .3);//calcula la calificacion parcial
         return (int) (firstPartialGrade);//se convierte a entero antes de devolver el resultado
     }
 
-    public int calculateFinalGrade(int firstPartial, int secondPartial, int thirdPartial, int finalExam) {//recibe 3 parametros para obtener la calificacion final
-        double averagePartials = (firstPartial + secondPartial + thirdPartial) / 3.0;
-        double finalGrade = (averagePartials * 0.5) + (finalExam * 0.5);
+    public int calculateFinalGrade(int firstPartial, int secondPartial, int thirdPartial, int finalExam) {//recibe 4 parametros para obtener la calificacion final
+        double averagePartials = (firstPartial + secondPartial + thirdPartial) / 3;
+        double finalGrade = (averagePartials * .5) + (finalExam * .5);
         return (int) (finalGrade); //se convierte a entero antes de devolver el resultado
     }
 
     public String checkFailureByAbsences(int totalHours, int absences) {
         double absencePercentage = (absences / (double) totalHours) * 100; //calcula el porcentaje de ausencias
-        if (absencePercentage > 10) {//calcula si el numero de faltas es mayor al 10%
-            return "el alumno esta reprobado por faltas.";
+        if (absencePercentage > 10) {//ciclo para calcular si el numero de faltas es mayor o menor al 10%
+            return "El alumno esta reprobado por faltas.";
         } else {
             return "Alumno cumple con las asistencias minimas.";
         }
