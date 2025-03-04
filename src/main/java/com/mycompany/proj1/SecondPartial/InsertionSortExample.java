cd0f0eb089ce7c6d0e14065b3a60d4d17da29645
cristhian <15186@ulsachihuahua.edu.mx>
2025-03-04 10:44:58 -0600

    nuevo proyecto de insertion y selection sort

---
+++ src/main/java/com/mycompany/proj1/SecondPartial/InsertionSortExample.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proj1.SecondPartial;
+
+/**
+ *
+ * @author DELL
+ */
+public class InsertionSortExample {
+    public static int insertionSort(int arr[]) {
+        int n = arr.length;
+        int operaciones = 0;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                operaciones++;
+            }
+            arr[j + 1] = key;
+        }
+        return operaciones;
+    }
+}
