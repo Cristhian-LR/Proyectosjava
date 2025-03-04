497e1e31c5fa8544ee965991cedae18cc2af4937
cristhian <15186@ulsachihuahua.edu.mx>
2025-03-04 10:44:31 -0600

    nuevo proyecto de insertion y selection sort

--- src/main/java/com/mycompany/proj1/EstructuraDeDatos.java
+++ src/main/java/com/mycompany/proj1/EstructuraDeDatos.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.proj1;
 
+import com.mycompany.proj1.SecondPartial.InsertionSortExample;
+
 
 /**
  *
@@ -14,8 +16,44 @@ package com.mycompany.proj1;
 public class EstructuraDeDatos {
 
     public static void main(String[] args) {
-        SalesWithDiscount objSalesWithDiscount = new SalesWithDiscount();
-        objSalesWithDiscount.totalWithDiscount(args);
+        
+        int peorCaso[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int mejorCaso[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        System.out.println("Peor Caso:");
+        int operacionesPeor = SelectionSortExample.selectionSort(peorCaso);
+        for (int num : peorCaso) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nOperaciones realizadas: " + operacionesPeor);
+        
+        System.out.println("\nMejor Caso:");
+        int operacionesMejor = SelectionSortExample.selectionSort(mejorCaso);
+        for (int num : mejorCaso) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nOperaciones realizadas: " + operacionesMejor);
+        
+        /*int peorCaso[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int mejorCaso[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        System.out.println("Peor Caso:");
+        int operacionesPeor = InsertionSortExample.insertionSort(peorCaso);
+        for (int num : peorCaso) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nOperaciones realizadas: " + operacionesPeor);
+        
+        System.out.println("\nMejor Caso:");
+        int operacionesMejor = InsertionSortExample.insertionSort(mejorCaso);
+        for (int num : mejorCaso) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nOperaciones realizadas: " + operacionesMejor);
+        
+        
+        /*SalesWithDiscount objSalesWithDiscount = new SalesWithDiscount();
+        System.out.println("el total es: "+ objSalesWithDiscount.totalWithDiscount());
         
         /*GradeCalculationPartial objGradeCalculationPartial = new GradeCalculationPartial();
         objGradeCalculationPartial.averageStatusFinal(args);
